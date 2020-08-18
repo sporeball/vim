0b00082f002f9477e245890d79ac91d52cb6aeff
sporeball <sporeballdev@gmail.com>
2020-08-18 16:52:55 -0700

    fix grid edges

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", victus.setup({
   id: "canvas",
-  w: 550,
-  h: 550,
+  w: 552,
+  h: 552,
   color: "#fff"
 }));
 
@@ -11,18 +11,18 @@ const ctx = x();
 var playerCount = 0;
 var currentPlayer = 1;
 
-var player1 = new victus.Ellipse(260, 260, 5, 5, "#ea323c"),
-  player2 = new victus.Ellipse(290, 260, 5, 5, "#0098dc"),
-  player3 = new victus.Ellipse(260, 290, 5, 5, "#5ac54f"),
-  player4 = new victus.Ellipse(290, 290, 5, 5, "#ffc825");
+var player1 = new victus.Ellipse(261, 261, 5, 5, "#ea323c"),
+  player2 = new victus.Ellipse(291, 261, 5, 5, "#0098dc"),
+  player3 = new victus.Ellipse(261, 291, 5, 5, "#5ac54f"),
+  player4 = new victus.Ellipse(291, 291, 5, 5, "#ffc825");
 
 var loadSound = new victus.Sound("res/audio/load.ogg", 0.5),
   uiSound = new victus.Sound("res/audio/ui.wav", 0.5),
   startSound = new victus.Sound("res/audio/start.wav", 1),
   selectSound = new victus.Sound("res/audio/select.ogg", 0.5),
   rollSound = new victus.Sound("res/audio/roll.ogg", 1);
-  
-// var locations = ownership = vim = Array(11).fill(0).map(x => Array(11).fill(0));  
+
+// var locations = ownership = vim = Array(11).fill(0).map(x => Array(11).fill(0));
 
 var direction = sign = null;
 
@@ -40,7 +40,7 @@ gesture = () => {
       init();
     }, 500)
 }
-    
+
 init = () => {
   setTimeout(() => {
     document.getElementById("subtitle").classList.remove("d-none");
@@ -63,7 +63,7 @@ init = () => {
 // start game with a certain number of players
 start = players => {
   playerCount = players;
-  
+
   switch (players) {
     case 2:
       player3.hide();
@@ -77,15 +77,15 @@ start = players => {
       document.getElementById("player_3").classList.remove("d-none");
       document.getElementById("player_4").classList.remove("d-none");
   }
-  
+
   document.getElementById(`player_${playerCount}`).classList.add("player-last");
   document.getElementById("player_count").innerHTML = `(${playerCount}-player)`;
 
   document.getElementById("start").classList.add("d-none");
   document.getElementById("game").classList.remove("d-none");
-  
+
   startSound.play();
-  
+
   window.requestAnimationFrame(loop);
 }
 
@@ -95,7 +95,7 @@ grid = () => {
       ctx.beginPath();
       ctx.strokeStyle = "#ccc";
       ctx.lineWidth = "1";
-      ctx.rect(i * 50, j * 50, 50, 50);
+      ctx.rect(i * 50 + 1, j * 50 + 1, 50, 50);
       ctx.stroke();
     }
   }
@@ -103,48 +103,48 @@ grid = () => {
 
 loop = () => {
   victus.clear();
-  
+
   grid();
   player1.draw();
   player2.draw();
   player3.draw();
   player4.draw();
-  
+
   window.requestAnimationFrame(loop);
 }
 
 chooseDirection = choice => {
   direction = choice;
-  
-  document.getElementById(choice).classList.add("active");  
-  
+
+  document.getElementById(choice).classList.add("active");
+
   setTimeout(() => {
     document.getElementById(choice).classList.remove("active");
     document.getElementById("direction_container").classList.add("d-none");
     document.getElementById("sign_container").classList.remove("d-none");
   }, 750)
-  
+
   selectSound.play();
 }
 
 chooseSign = choice => {
   sign = choice;
-  
+
   document.getElementById(choice).classList.add("active");
-  
+
   setTimeout(() => {
     document.getElementById(choice).classList.remove("active");
     document.getElementById("sign_container").classList.add("d-none");
     document.getElementById("roll_container").classList.remove("d-none");
   }, 750)
-  
+
   selectSound.play();
 }
 
 roll = () => {
   // disable the button for the die
   document.getElementById("roll").setAttribute("disabled", "");
-  
+
   rollSound.play();
 
   // start to change the face of the die
@@ -157,7 +157,7 @@ roll = () => {
   setTimeout(() => {
     clearInterval(face);
   }, 2000);
-  
+
   setTimeout(() => {
     document.getElementById("roll").classList.add("hover");
   }, 2000);
@@ -175,7 +175,7 @@ postRoll = () => {
   currentPlayer != playerCount ? currentPlayer++ : currentPlayer = 1;
 
   document.getElementById("turn").innerHTML = `player ${currentPlayer}'s turn`
-  
+
   document.getElementById("roll_container").classList.add("d-none");
   document.getElementById("direction_container").classList.remove("d-none");
 }
